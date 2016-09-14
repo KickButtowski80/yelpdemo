bd14baac5d2cd929593c797303404da55792ee34
kickbuttowski59 <pazpaz22@yahoo.com>
2016-09-14 21:18:36 +0000

    :(

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module Yelpdemo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    puts config.method(:raise_in_transactional_callbacks=).source_location
+   # config.active_record.raise_in_transactional_callbacks = true
+   # puts config.method(:raise_in_transactional_callbacks=).source_location
     
   end
 end
