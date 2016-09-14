aebbb5c26979db41dc5a0eed32d77f6d8f0db5e7
kickbuttowski59 <pazpaz22@yahoo.com>
2016-09-14 21:30:44 +0000

    readd will paginate gem

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'devise'
 gem 'mini_magick'
 gem 'fog-aws'
 gem 'searchkick'
- 
+gem 'will_paginate' 
  
 group :development do
   gem 'sqlite3'
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,7 +14,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant..paginate(:page => params[:page], :per_page => 2)
   end
 
   # GET /restaurants/1
